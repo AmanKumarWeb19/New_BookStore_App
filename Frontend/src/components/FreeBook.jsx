48be2b7b7ba24b1c50a71e2eca6df57868781941
Aman Kumar <akgupta0524@gmail.com>
2024-12-19 13:14:56 +0530

    use backend api

--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from "../../public/list.json";
+import axios from "axios";
 import Cards from "./Cards";
 
 const FreeBook = () => {
-  const filteredData = list.filter((data) => data.category === "Free");
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
   var settings = {
     dots: true,
     infinite: false,
@@ -55,8 +69,8 @@ const FreeBook = () => {
         </div>
         <div>
           <Slider {...settings}>
-            {filteredData.map((item) => (
-              <Cards item={item} key={item.id} />
+            {book.map((item) => (
+              <Cards item={item} key={item._id} />
             ))}
           </Slider>
         </div>
