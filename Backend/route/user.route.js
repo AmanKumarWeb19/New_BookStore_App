0d2b06e1622a59b53a3166e7daeb0e0db33dab65
Aman Kumar <akgupta0524@gmail.com>
2024-12-19 16:22:05 +0530

    add user routes in route file

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { signup } from "../Controller/user.controller.js";
+import { login } from "../Controller/user.controller.js";
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
+userRouter.post("/login", login);
 
 export default userRouter;
