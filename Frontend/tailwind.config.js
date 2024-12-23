e44643837eb9b30b61c6e5c9cd2815c6c73e55ef
Aman Kumar <akgupta0524@gmail.com>
2024-12-23 16:36:53 +0530

    update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -5,7 +7,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light"], // Explicitly set the theme to light
   },
