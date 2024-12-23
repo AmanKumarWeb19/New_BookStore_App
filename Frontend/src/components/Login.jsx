5db3f1cb3eed65a70d19a80c70995d29468ab3b2
Aman Kumar <akgupta0524@gmail.com>
2024-12-23 15:15:55 +0530

    add backend deployed link

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -17,7 +17,10 @@ const Login = () => {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post(
+        "https://new-bookstore-app-backend-hjhc.onrender.com/user/login",
+        userInfo
+      )
       .then((res) => {
         console.log(res.data);
         if (res.data) {
--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -24,7 +24,10 @@ const SignUp = () => {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post(
+        "https://new-bookstore-app-backend-hjhc.onrender.com/user/signup",
+        userInfo
+      )
       .then((res) => {
         console.log(res.data);
         if (res.data) {
